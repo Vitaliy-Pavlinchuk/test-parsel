a87ccfc63e6e3161edfe16f9e5ece995d6d04f69
Vitaliy-Pavlinchuk <vitaliy.pavlinchuk@gmail.com>
2022-05-05 11:57:26 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+const button = document.querySelector('button');
 
-console.log(galleryItems);
+const lastItem = document.createElement('button');
+lastItem.textContent = 'cleak me';
+lastItem.style.backgroundColor = 'green'
+document.querySelector('.gallery').append(lastItem);
+
+
+
+const onBtn = () => {
+    setTimeout(() => 
+    alert("I love async JS!")
+  , 2000);
+}
+
+lastItem.addEventListener('click', onBtn)
+
+
+
+
+
+
+const PROMT_DELAY = 1009
+const MAX_PROMT_DALAY = 3
+
+let promtCounter = 0
+let hasSubscriebd = false
+
+const intervalID =  setInterval(() => {
+    if (promtCounter === MAX_PROMT_DALAY) {
+        console.log('нужно остановить');
+        clearInterval(intervalID)
+        return
+    } console.log('Подпишись на рассилку' + Date.now());
+    promtCounter += 1
+}, PROMT_DELAY )
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
 
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const KEY = 'videoplayer-current-time';
+
+// const player = new Player('vimeo-player');
+// const key = localStorage.getItem(KEY);
+// if (key) {
+//   player.setCurrentTime(parseFloat(key));
+// }
+
+// player.on(
+//   'timeupdate',
+//   throttle(data => {
+//     localStorage.setItem(KEY, data.seconds.toString());
+//   }, 1000),
+// );
+
+
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+// player.on('timeupdate',
+//   throttle(function (timeupdate) {
+//     localStorage.setItem(LOCALSTORAGE_KEY, timeupdate.seconds);
+//   }, 1000),
+// );
+
+// player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
